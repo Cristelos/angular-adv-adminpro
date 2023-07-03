9359e5bc7557fbec6494ddc8a0ea3c57446cc69f
Patricia Fernandez <pfernandez@nettrim.es>
2023-07-03 09:01:08 +0200

    adding a donut visual

---
+++ src/app/components/donut/donut.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import { ChartData, ChartEvent, ChartType } from 'chart.js';
+
+
+@Component({
+  selector: 'app-donut',
+  templateUrl: './donut.component.html',
+  styles: [
+  ]
+})
+export class DonutComponent {
+
+  // Doughnut
+  public doughnutChartLabels: string[] = [ 'Download Sales', 'In-Store Sales', 'Mail-Order Sales' ];
+  public doughnutChartData: ChartData<'doughnut'> = {
+    labels: this.doughnutChartLabels,
+    datasets: [
+      { data: [ 350, 450, 100 ] },
+    ]
+  };
+
+}
--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SharedModule } from '../shared/shared.module';
+import { FormsModule } from '@angular/forms';
+
 
 //Modulos
 import { RouterModule } from '@angular/router';
@@ -11,7 +13,6 @@ import { ProgressComponent } from './progress/progress.component';
 import { Grafica1Component } from './grafica1/grafica1.component';
 import { PagesComponent } from './pages.component';
 import { AppRoutingModule } from '../app-routing.module';
-import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
