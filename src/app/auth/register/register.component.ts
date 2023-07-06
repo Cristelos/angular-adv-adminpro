642355cbca4b70a0e9c24cb1d6fef354c65773f0
Patricia Fernandez <pfernandez@nettrim.es>
2023-07-06 09:00:37 +0200

    validaciones del formulario de registro

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -6,5 +7,56 @@ import { Component } from '@angular/core';
   styleUrls: [ './register.component.css' ]
 })
 export class RegisterComponent {
+
+  public formSubmitted = false;
+
+  public registerForm = this.fb.group({
+    nombre: ['', [ Validators.required, Validators.minLength(3)] ],
+    email: ['', [ Validators.required, Validators.email]],
+    password: ['', [ Validators.required]],
+    password2: ['', [ Validators.required]],
+    terminos: [false, [ Validators.required]],
+  });
+
+  constructor ( private fb: FormBuilder ){}
+
+  crearUsuario(){
+    this.formSubmitted = true;
+    console.log(this.registerForm.value);
+
+    if ( this.registerForm.valid ) {
+      console.log('posteando formulario');
+    } else {
+      console.log('El formulario no es correcto...');
+      
+    }
+    
+  }
+
+  campoNoValido( campo: string ): boolean {
+
+    if ( this.registerForm.get(campo)?.invalid && this.formSubmitted ){
+      return true;
+    } else {
+      return false;
+    }
+
+  }
+
+  contrasenasNoValidas() {
+    const pass1 = this.registerForm.get('password')?.value;
+    const pass2 = this.registerForm.get('password2')?.value;
+
+    if ( (pass1 !== pass2) && this.formSubmitted ) {
+      return true;
+    } else {
+      return false;
+    }
+
+  }
+
+  aceptaTerminos(){
+    return !this.registerForm.get('terminos')?.value && this.formSubmitted;
+  }
 
 }
